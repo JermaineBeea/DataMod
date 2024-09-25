ac319faf41e1030d82477aa878aac9e802df2a44
wtc <wtc@pop-os.localdomain>
2024-09-25 16:20:02 +0200

    saved

--- Uno_Game/mainUno.py
+++ Uno_Game/mainUno.py
@@ -3,11 +3,17 @@ from Modules.RandGenerator import randId
 
 
 # Variables for cards 
+
+colour = ['red','blue','yellow','white']
 card_num = list(range(10))
 wild_card = ['W_1', 'W_2', 'W_3']
-colour = ['red','blue','yellow','white']
 action_cards = ['act_1', 'act_2', 'act_3']
 wild_card = ['W_1', 'W_2', 'W_3']
+
+#TYPES OF CARDS
+# colour & num
+# Action card & colour
+# Wild card isnt mapped with any other variable
 
 num_drawn_cards = 4
 used_Id = []
@@ -18,7 +24,7 @@ for player, cards in player_names.items():
     rand_choice = randId(card_num, colour, exclude = used_Id)
     used_Id.append(rand_choice[0]) 
     cards.append(rand_choice[1])
-		
+
 print (f'{player_names}\n')
 print(used_Id)
 
