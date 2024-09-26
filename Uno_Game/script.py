34eb7517abb3e6fab685c793979a776bcecf9ce3
wtc <wtc@pop-os.localdomain>
2024-09-26 09:33:14 +0200

    ok

--- Uno_Game/script.py
+++ Uno_Game/script.py
@@ -3,5 +3,7 @@ import numpy as np
 
 player = {'dave': [(1, 'white'), (0, 'red'), (2, 'blue'), (9, 'white')], 'susan': [(3, 'blue'), (0, 'blue'), (6, 'red'), ('act_3', 'blue')], 'mike': [(7, 'yellow'), (0, 'red'), (0, 'white'), (3, 'blue')]}
 
-def sourceFunction ():
+def sourceFunction (name):
+  cards = player[name]
+  print(f'Card')
   
