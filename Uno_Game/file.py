ee6c97892407186bdea86f57a95595a21b5a05e8
wtc <wtc@pop-os.localdomain>
2024-09-26 08:13:51 +0200

    ok

--- Uno_Game/file.py
+++ Uno_Game/file.py
@@ -1,11 +1,11 @@
 my_list = [1, 2, 3, 4]
 my_iter = iter(my_list)
 
-print(next(my_iter))  # Output: 1
+print(next(my_iter))  
 
 
-def read_next_line():
-    return input("Enter a line (or type 'end' to stop): ")
+# def read_next_line():
+#     return input("Enter a line (or type 'end' to stop): ")
 
-for line in iter(read_next_line, 'end'):
-    print(f"Got line: {line}")
+# for line in iter(read_next_line, 'end'):
+#     print(f"Got line: {line}")
