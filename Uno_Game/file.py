148b61d6b28add3aba8c3793c88d6235a1655ea4
wtc <wtc@pop-os.localdomain>
2024-09-26 13:15:09 +0200

    ok

--- Uno_Game/file.py
+++ Uno_Game/file.py
@@ -4,6 +4,9 @@ import tkinter
 root = tkinter.Tk()
 
 frame = tkinter.Frame(root, bg = 'red')
+frame.pack(pady = 10)
 
-label = tkinter.Label(frame)
+label = tkinter.Label(frame, text= 'Lable')
+label.pack(pady = 10)
+
 root.mainloop()
