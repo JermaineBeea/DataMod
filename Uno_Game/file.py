2c479141e55a4e67f6ebb11b99bce3744bab2fa5
wtc <wtc@pop-os.localdomain>
2024-09-26 16:30:23 +0200

    ok

--- Uno_Game/file.py
+++ Uno_Game/file.py
@@ -1,8 +1,9 @@
 import tkinter
 
-
 def displayCards (cards):
-  """"
+  """" 
+  Function to display cards 
+  """
   root = tkinter.Tk()
   root.geometry('400x550')
   frame_back_colour = 'darkgrey'
@@ -11,7 +12,7 @@ def displayCards (cards):
     card_colour = card[1]
     card_text = card[0]
 
-    frame = tkinter.Frame(root, bg = frame_back_colour, width = 100, height = 150, highlightbackground='black', highlightthickness=2, bd=2)
+    frame = tkinter.Frame(root, bg = frame_back_colour, width = 100, height = 150, highlightbackground='black', highlightthickness=2, bd=1)
     frame.pack_propagate(False)
 
     card_button = tkinter.Button(frame, text= f'{card_text}', bg = card_colour, fg = 'white', font=('Arial', 24))
