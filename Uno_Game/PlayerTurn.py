3b02157e7f4ed3e96e6b14aa6caa3384014f15cc
wtc <wtc@pop-os.localdomain>
2024-09-26 10:47:01 +0200

    ok

--- Uno_Game/PlayerTurn.py
+++ Uno_Game/PlayerTurn.py
@@ -16,33 +16,32 @@ def sourceFunction(name):
     cards = players[name]
     print(f'Cards for player {name} \nare {cards}')
 
-def playerTurn ():
+num_players = len(players)
 
-  # Global Button configuration variables
-  button_width = 20  
-  button_height = 2  
-  button_colour = 'darkorange'
-  text_colour = 'white'
+# Global Button configuration variables
+button_width = 20  
+button_height = 2  
+button_colour = 'darkblue'
+text_colour = 'white'
 
-  # Root dimensions based on the number of players and button dimensions
-  root_height = 400
-  root_width = 200
+# Root dimensions based on the number of players and button dimensions
+root_height = 400
+root_width = 200
 
-  # Initialize the root window
-  root = tkinter.Tk()
-  root.title('Players')
-  root.config(bg='darkgrey')
+# Initialize the root window
+root = tkinter.Tk()
+root.title('Players')
+root.config(bg='darkgrey')
 
-  # Center the window
-  centerWidget(root, root_width, root_height)
+# Center the window
+centerWidget(root, root_width, root_height)
 
-  # Create and pack buttons for each player
-  for player_name in players.keys():
-      func = partial(sourceFunction, name=player_name)
-      button = tkinter.Button(root, command=func, text=f'{player_name} \nCards', font = ('Consolas', 14), bg = button_colour, fg = text_colour)
-      button.config(width=button_width, height=button_height)
-      button.pack(fill= 'both',  expand= True) 
+# Create and pack buttons for each player
+for player_name in players.keys():
+    func = partial(sourceFunction, name=player_name)
+    button = tkinter.Button(root, command=func, text=f'{player_name} \nCards', font = ('Consolas', 14), bg = button_colour, fg = text_colour)
+    button.config(width=button_width, height=button_height)
+    button.pack(fill= 'both',  expand= True) 
 
-  # Start the main loop
-  root.mainloop()
-
+# Start the main loop
+root.mainloop()
