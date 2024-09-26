e6b5af0aa72cd49b933466cb478eb8b5683ea9f5
wtc <wtc@pop-os.localdomain>
2024-09-26 16:06:04 +0200

    ok

--- Uno_Game/file.py
+++ Uno_Game/file.py
@@ -7,7 +7,7 @@ root.geometry('400x550')
 
 
 frame_back_colour = 'darkgrey'
-for n, card in cards:
+for n,card in enumerate(cards):
   card_colour = card[1]
   card_text = card[0]
 
@@ -18,17 +18,5 @@ for n, card in cards:
   card_button.pack(fill = 'both', expand = True)
 
   frame.grid(row=n//3, column=n%3, padx=10, pady=10)
-
-# # FRAME 2
-# # Adding black border with highlightthickness and bd
-# frame_back_colour = 'darkgrey'
-# frame2 = tkinter.Frame(root, bg = frame_back_colour, width=100, height=200, highlightbackground='black', highlightthickness=2, bd=2)
-# frame2.pack_propagate(False)
-
-# label = tkinter.Label(frame2, text= f'2', bg = 'red', font=('Arial', 24))
-# label.pack(fill = 'both', expand = True)
-
-# frame2.grid(row= 3, column=2, padx=10, pady=10)
-
 
 root.mainloop()
