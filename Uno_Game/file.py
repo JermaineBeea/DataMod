2094490790280ecfd768fce439cd8b720e46f958
wtc <wtc@pop-os.localdomain>
2024-09-26 14:53:26 +0200

    ok

--- Uno_Game/file.py
+++ Uno_Game/file.py
@@ -6,11 +6,12 @@ root = tkinter.Tk()
 root.geometry('300x300')
  
 # Adding black border with highlightthickness and bd
-frame = tkinter.Frame(root, bg='blue', width=200, height=100, 
+frame_back_colour = 'darkgrey'
+frame = tkinter.Frame(root, bg = frame_back_colour, width=200, height=100, 
 highlightbackground='black', highlightthickness=2, bd=2)
 frame.pack(fill='both', expand=True, pady=10)
 
-label = tkinter.Label(frame, text= 'Frame label', bg='red', font=('Arial', 24))
+label = tkinter.Label(frame, text= 'Frame label', bg= 'blue', font=('Arial', 24))
 label.pack(pady=10)
 
 root.mainloop()
