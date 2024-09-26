50cd31666dbcb0834e51e90d46fdca5cbdf10e2d
wtc <wtc@pop-os.localdomain>
2024-09-26 09:29:51 +0200

    ok

--- Uno_Game/script.py
+++ Uno_Game/script.py
@@ -1,6 +1,8 @@
 import tkinter
+import numpy as np
 
-player = {"dave": [('1','red'), ('2', 'blue')]}
+player = {'dave': [(np.str_('5'), 'yellow'), ('act_1', 'yellow'), ('act_2', 'blue'), (np.str_('6'), 'blue')], 'susan': [(np.str_('0'), 'yellow'), ('act_1', 'yellow'), (np.str_('8'), 'blue'), (np.str_('6'), 'white')], 'mathew': [['W_1', 'brown'], (np.str_('4'), 'blue'), (np.str_('1'), 'yellow'), (np.str_('3'), 'white')], 'peter': [(np.str_('7'), 'white'), (np.str_('6'), 'yellow'), ('act_1', 'white'), ('act_3', 'yellow')]}
+
 
 def sourceFunction ():
   
