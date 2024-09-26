4069ffb3c939aad694aedd570b90b950ae84b993
wtc <wtc@pop-os.localdomain>
2024-09-26 07:24:42 +0200

    ok

--- Uno_Game/script.py
+++ Uno_Game/script.py
@@ -12,11 +12,13 @@ def centerWidget(widget_root, root_width, root_height, hoizontal_shift, vertical
 lst = (8, 'red'), (6, 'yellow'), (8, 'blue'), (2, 'yellow')
 
 root_width = 300
-root_height = 
+root_height = 300
+h_shift = 100
+v_shift = 100
 
 root = tkinter.Tk()
 root.title('Player Card')
-ro
+centerWidget(root, root_width, root_height)
 
 
 
