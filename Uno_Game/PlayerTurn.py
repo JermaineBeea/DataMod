8631de3bce64f987c55b3e0b805e7d5de5525c80
wtc <wtc@pop-os.localdomain>
2024-09-26 14:42:38 +0200

    ok

--- Uno_Game/PlayerTurn.py
+++ Uno_Game/PlayerTurn.py
@@ -4,6 +4,13 @@ from Modules.GeneratePlayers import centerWidget
 from Modules.DrawCard import players_cards
 from functools import partial
 
+player_cards = {'dave': [(1, 'blue'), (9, 'blue'), (0, 'blue'), (7, 'blue'), (8, 'white'), (5, 'yellow'), ('act_2', 'white')], 
+                'smith': [(0, 'blue'), (5, 'white'), (0, 'white'), ('act_2', 'red'), (0, 'red'), (8, 'blue'), ('act_2', 'blue')], 
+                'peter': [(2, 'white'), ('act_2', 'blue'), (5, 'white'), (6, 'white'), (9, 'yellow'), ('act_3', 'white'), (1, 'blue')], 
+                'david': [(2, 'blue'), (0, 'blue'), ('act_2', 'white'), ('act_1', 'yellow'), (2, 'blue'), ('act_3', 'white'), ('act_3', 'red')], 
+                'peter%3': [['W_3', 'brown'], (9, 'white'), (4, 'white'), (4, 'blue'), ['W_1', 'brown'], (7, 'blue'), (1, 'blue')], 
+                'king': [(5, 'blue'), (7, 'white'), (0, 'yellow'), (7, 'yellow'), (8, 'red'), (6, 'red'), ['W_1', 'brown']], 
+                'kong': [('act_3', 'white'), (0, 'yellow'), ['W_1', 'brown'], (0, 'white'), ['W_2', 'brown'], ('act_1', 'yellow'), (1, 'red')], 'rad': [(3, 'red'), (0, 'white'), ['W_1', 'brown'], (8, 'red'), (8, 'red'), (0, 'red'), ('act_2', 'white')]}
 
 # Player cards is fetched from DrawCard Module
 def sourceFunction(name):
