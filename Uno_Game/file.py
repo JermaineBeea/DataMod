5d2c03397000f9a5047ae954bee1f911f9ff1a48
wtc <wtc@pop-os.localdomain>
2024-09-26 13:20:27 +0200

    ok

--- Uno_Game/file.py
+++ Uno_Game/file.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 
 
-
+cards = [(9, 'white'), (1, 'white'), ['W_2', 'brown'], ('act_1', 'white'), (1, 'white'), (3, 'white'), ('act_2', 'white')]
 root = tk.Tk()
 root.geometry('300x300')
 
