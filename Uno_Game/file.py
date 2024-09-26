df77b48ea44ae2e4ebfa5c2bba3d4f3aa314b19d
wtc <wtc@pop-os.localdomain>
2024-09-26 13:22:37 +0200

    ok

--- Uno_Game/file.py
+++ Uno_Game/file.py
@@ -6,8 +6,11 @@ cards = [(9, 'white'), (1, 'white'), ['W_2', 'brown'], ('act_1', 'white'), (1, '
 root = tk.Tk()
 root.geometry('300x300')
 
-for car
-frame = tk.Frame(root, bg='red', width=200, height=100)
-frame.pack(fill = 'both', expand= True, pady=10)
+for card in cards:
+  colour = card[1].lower()
+  frame = tk.Frame(root, bg= colour, width=200, height=100)
+  frame.pack(fill = 'both', expand= True, pady=10)
+
+
 
 root.mainloop()
