49b25e7562e1f838777e6fa507d189db2228bdd6
wtc <wtc@pop-os.localdomain>
2024-09-26 13:03:50 +0200

    ok

--- Uno_Game/PlayerTurn.py
+++ Uno_Game/PlayerTurn.py
@@ -32,7 +32,7 @@ centerWidget(root, root_width, root_height)
 # Create and pack buttons for each player
 for player_name in players_cards.keys():
     func = partial(sourceFunction, name=player_name)
-    button = tkinter.Button(root, command=func, text=f'{player_name} \nCards', font = ('Consolas', 14), bg = button_colour, fg = text_colour)
+    button = tkinter.Button(root, command=func, text=f'{player_name.upper()} display \nCards', font = ('Consolas', 14), bg = button_colour, fg = text_colour)
     button.config(width=button_width, height=button_height)
     button.pack(fill= 'both',  expand= True) 
 
