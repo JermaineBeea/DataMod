b392e409e8026617236c5315cb58d266db3e9727
JermaineBeea <jermainebeea@gmail.com>
2024-09-26 21:25:15 +0200

    Automated push at 2024-09-26-21-25-14

--- Uno_Game/DisplayCard.py
+++ Uno_Game/DisplayCard.py
@@ -1,13 +1,12 @@
 import tkinter
 from Modules.GeneratePlayers import centerWidget
 
-# Initialize current_root globally to track the current window
+# current_root to track the current window open when Player button is pressed inPlayerTurn
 current_root = None
 
 def displayCards(cards):
     global current_root
     
-    # Destroy the previous root window if it exists
     if current_root is not None:
         current_root.destroy()
     
@@ -16,29 +15,23 @@ def displayCards(cards):
     x_shift = 200
     y_shift = -200
     
-    # Create a new root window
     root = tkinter.Tk()
     frame_back_colour = 'darkgrey'
     
-    # Center the new window
     centerWidget(root, root_width, root_height, x_shift, y_shift)
     
-    # Create and display frames for cards
     for n, card in enumerate(cards):
         card_colour = card[1]
         card_text = card[0]
         
-        frame = tkinter.Frame(root, bg=frame_back_colour, width=100, height=150, 
-                              highlightbackground='black', highlightthickness=2, bd=1)
+        frame = tkinter.Frame(root, bg=frame_back_colour, width=100, height=150, highlightbackground='black', highlightthickness=2, bd=1)
         frame.pack_propagate(False)
 
-        card_button = tkinter.Button(frame, text=f'{card_text}', bg=card_colour, 
-                                     fg='white', font=('Arial', 24))
+        card_button = tkinter.Button(frame, text=f'{card_text}', bg=card_colour, fg='white', font=('Arial', 24))
         card_button.pack(fill='both', expand=True)
 
         frame.grid(row=n//3, column=n%3, padx=10, pady=10)
     
-    # Update current_root to the new root window
     current_root = root
     
     root.mainloop()
--- Uno_Game/PlayerTurn.py
+++ Uno_Game/PlayerTurn.py
@@ -19,32 +19,26 @@ from functools import partial
 def sourceFunction(cards_):
     displayCards(cards_)
 
-# Global Button configuration variables
 button_width = 20  
 button_height = 4  
 button_colour = 'darkred'
 text_colour = 'white'
 
-# Root dimensions based on the number of players and button dimensions
 root_height = 400
 root_width = 200
 x_shit = - 600
 y_shift = - 200
 
-# Initialize the root window
 root = tkinter.Tk()
 root.title('CHOOSE A PLAYER TO GO NEXT')
-root.config(bg='darkgrey')
+root.config(bg = 'darkgrey')
 
-# Center the window
 centerWidget(root, root_width, root_height, x_shit, y_shift)
 
-# Create and pack buttons for each player
 for player_name, cards_drawn in players_cards.items():
     func = partial(sourceFunction, cards_drawn)
     button = tkinter.Button(root, command=func, text=f'{player_name.upper()} \n Display \nCards', font = ('Consolas', 14), bg = button_colour, fg = text_colour)
     button.config(width=button_width, height=button_height)
     button.pack(fill= 'both',  expand= True) 
 
-# Start the main loop
 root.mainloop()
