a640f51fa7a1eeb4cead35cfcbdaa36e66139b5a
wtc <wtc@pop-os.localdomain>
2024-09-26 09:35:28 +0200

    ok

--- Uno_Game/script.py
+++ Uno_Game/script.py
@@ -1,4 +1,5 @@
 import tkinter
+import par
 import numpy as np
 
 player = {'dave': [(1, 'white'), (0, 'red'), (2, 'blue'), (9, 'white')], 'susan': [(3, 'blue'), (0, 'blue'), (6, 'red'), ('act_3', 'blue')], 'mike': [(7, 'yellow'), (0, 'red'), (0, 'white'), (3, 'blue')]}
@@ -11,5 +12,7 @@ def sourceFunction (name):
 root = tkinter.Tk()
 root.title('Players')
 
+for player_name, cards in player.items():
 
+    func = partial
   
