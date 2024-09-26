fb809dee9e6d58288bae337a66fc24f06ea8ba9b
wtc <wtc@pop-os.localdomain>
2024-09-26 14:45:54 +0200

    ok

--- Uno_Game/PlayerTurn.py
+++ Uno_Game/PlayerTurn.py
@@ -5,21 +5,20 @@ from Modules.GeneratePlayers import centerWidget
 from functools import partial
 
 players_cards = {
-    'dave': [(1, 'blue'), (9, 'blue'), (0, 'blue'), (7, 'blue'), (8, 'white'), (5, 'yellow'), ('act_2', 'white')], 
-                'smith': [(0, 'blue'), (5, 'white'), (0, 'white'), ('act_2', 'red'), (0, 'red'), (8, 'blue'), ('act_2', 'blue')], 
-                'peter': [(2, 'white'), ('act_2', 'blue'), (5, 'white'), (6, 'white'), (9, 'yellow'), ('act_3', 'white'), (1, 'blue')], 
-                'david': [(2, 'blue'), (0, 'blue'), ('act_2', 'white'), ('act_1', 'yellow'), (2, 'blue'), ('act_3', 'white'), ('act_3', 'red')], 
-                'peter%3': [['W_3', 'brown'], (9, 'white'), (4, 'white'), (4, 'blue'), ['W_1', 'brown'], (7, 'blue'), (1, 'blue')], 
-                'king': [(5, 'blue'), (7, 'white'), (0, 'yellow'), (7, 'yellow'), (8, 'red'), (6, 'red'), ['W_1', 'brown']], 
-                'kong': [('act_3', 'white'), (0, 'yellow'), ['W_1', 'brown'], (0, 'white'), ['W_2', 'brown'], ('act_1', 'yellow'), (1, 'red')], 
-                'rad': [(3, 'red'), (0, 'white'), ['W_1', 'brown'], (8, 'red'), (8, 'red'), (0, 'red'), ('act_2', 'white')]
-                }
+  'dave': [(1, 'blue'), (9, 'blue'), (0, 'blue'), (7, 'blue'), (8, 'white'), (5, 'yellow'), ('act_2', 'white')], 
+  'smith': [(0, 'blue'), (5, 'white'), (0, 'white'), ('act_2', 'red'), (0, 'red'), (8, 'blue'), ('act_2', 'blue')], 
+  'peter': [(2, 'white'), ('act_2', 'blue'), (5, 'white'), (6, 'white'), (9, 'yellow'), ('act_3', 'white'), (1, 'blue')], 
+  'david': [(2, 'blue'), (0, 'blue'), ('act_2', 'white'), ('act_1', 'yellow'), (2, 'blue'), ('act_3', 'white'), ('act_3', 'red')], 
+  'peter%3': [['W_3', 'brown'], (9, 'white'), (4, 'white'), (4, 'blue'), ['W_1', 'brown'], (7, 'blue'), (1, 'blue')], 
+  'king': [(5, 'blue'), (7, 'white'), (0, 'yellow'), (7, 'yellow'), (8, 'red'), (6, 'red'), ['W_1', 'brown']], 
+  'kong': [('act_3', 'white'), (0, 'yellow'), ['W_1', 'brown'], (0, 'white'), ['W_2', 'brown'], ('act_1', 'yellow'), (1, 'red')], 
+  'rad': [(3, 'red'), (0, 'white'), ['W_1', 'brown'], (8, 'red'), (8, 'red'), (0, 'red'), ('act_2', 'white')]
+  }
 
 # Player cards is fetched from DrawCard Module
 def sourceFunction(name):
     cards = players_cards[name]
     print(f'Cards for player {name} \nare {cards}')
-
 
 # Global Button configuration variables
 button_width = 20  
@@ -30,8 +29,8 @@ text_colour = 'white'
 # Root dimensions based on the number of players and button dimensions
 root_height = 400
 root_width = 200
-x_shit = -200
-y_shift = -100
+x_shit = -600
+y_shift = -200
 
 # Initialize the root window
 root = tkinter.Tk()
