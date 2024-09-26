1cc04928b663b58c240212902a3229771b339c22
wtc <wtc@pop-os.localdomain>
2024-09-26 09:47:51 +0200

    ok

--- Uno_Game/script.py
+++ Uno_Game/script.py
@@ -3,21 +3,22 @@ import numpy as np
 from Modules.GeneratePlayers import centerWidget
 from functools import partial
 
-player = {'dave': [(1, 'white'), (0, 'red'), (2, 'blue'), (9, 'white')], 'susan': [(3, 'blue'), (0, 'blue'), (6, 'red'), ('act_3', 'blue')], 'mike': [(7, 'yellow'), (0, 'red'), (0, 'white'), (3, 'blue')]}
+players = {'dave': [(1, 'white'), (0, 'red'), (2, 'blue'), (9, 'white')], 'susan': [(3, 'blue'), (0, 'blue'), (6, 'red'), ('act_3', 'blue')], 'mike': [(7, 'yellow'), (0, 'red'), (0, 'white'), (3, 'blue')]}
 
 def sourceFunction (name):
-  cards = player[name]
+  cards = players[name]
   print(f'Cards for player {name} \nare {cards}')
 
+num_players = len(players)
 
 # Root Main config variables
-root_width = 300
+root_height = 30*num_players
 root = tkinter.Tk()
 root.title('Players')
 root.config(bg = 'darkgrey')
 centerWidget(root, )
 
-for player_name, cards in player.items():
+for player_name, cards in players.items():
 
     func = partial(sourceFunction, name = player_name)
     button = tkinter.Button(root, command = func, text = f'Player {player_name} Cards', font = ('Consolas', 14), bg = 'darkred', fg = 'white')
