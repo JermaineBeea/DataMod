4d621aab420ecb0296ff7f283e7ff26557006327
wtc <wtc@pop-os.localdomain>
2024-09-26 09:36:34 +0200

    ok

--- Uno_Game/script.py
+++ Uno_Game/script.py
@@ -1,6 +1,7 @@
 import tkinter
-import par
 import numpy as np
+
+from functools import partial
 
 player = {'dave': [(1, 'white'), (0, 'red'), (2, 'blue'), (9, 'white')], 'susan': [(3, 'blue'), (0, 'blue'), (6, 'red'), ('act_3', 'blue')], 'mike': [(7, 'yellow'), (0, 'red'), (0, 'white'), (3, 'blue')]}
 
@@ -14,5 +15,6 @@ root.title('Players')
 
 for player_name, cards in player.items():
 
-    func = partial
+    func = partial(sourceFunction, name = player_name)
+    
   
