ec5fb191175a8d00c570b389e9f5ddaa9e2e8c43
wtc <wtc@pop-os.localdomain>
2024-09-26 15:22:49 +0200

    ok

--- Uno_Game/file.py
+++ Uno_Game/file.py
@@ -11,7 +11,7 @@ frame_back_colour = 'darkgrey'
 frame = tkinter.Frame(root, bg = frame_back_colour, width=200, height=100, 
 highlightbackground='black', highlightthickness=2, bd=2)
 # frame.pack(fill='both', expand= False, pady=10)
-label = tkinter.Label(frame, text= f'Frame\nlabel', bg = frame_back_colour, font=('Arial', 24))
+label = tkinter.Label(frame, text= f'Uno Card\n1', bg = frame_back_colour, font=('Arial', 24))
 label.pack(pady = 10)
 frame.grid(row= 2, column=3, padx=10, pady=10)
 
@@ -21,7 +21,7 @@ frame_back_colour = 'darkgrey'
 frame2= tkinter.Frame(root, bg = frame_back_colour, width=200, height=100, 
 highlightbackground='black', highlightthickness=2, bd=2)
 # frame.pack(fill='both', expand= False, pady=10)
-label = tkinter.Label(frame2, text= f'Frame_2\nlabel', bg = frame_back_colour, font=('Arial', 24))
+label = tkinter.Label(frame2, text= f'Uno Card\n2', bg = frame_back_colour, font=('Arial', 24))
 label.pack(pady = 10)
 frame2.grid(row= 3, column=2, padx=10, pady=10)
 
