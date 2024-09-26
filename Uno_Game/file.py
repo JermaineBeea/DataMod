6630ae7541cb2d5c7eb35107812813bc6edff434
wtc <wtc@pop-os.localdomain>
2024-09-26 13:21:30 +0200

    ok

--- Uno_Game/file.py
+++ Uno_Game/file.py
@@ -2,10 +2,12 @@ import tkinter as tk
 
 
 cards = [(9, 'white'), (1, 'white'), ['W_2', 'brown'], ('act_1', 'white'), (1, 'white'), (3, 'white'), ('act_2', 'white')]
+
 root = tk.Tk()
 root.geometry('300x300')
 
+for car
 frame = tk.Frame(root, bg='red', width=200, height=100)
-frame.pack(pady=10)
+frame.pack(fill = 'both', expand= True, pady=10)
 
 root.mainloop()
