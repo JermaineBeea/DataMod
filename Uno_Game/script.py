9fbf6d945ec073089a6238ad031c4b91d414fcff
wtc <wtc@pop-os.localdomain>
2024-09-26 10:41:29 +0200

    ok

--- Uno_Game/script.py
+++ Uno_Game/script.py
@@ -21,10 +21,12 @@ num_players = len(players)
 # Global Button configuration variables
 button_width = 20  
 button_height = 2  
+button_colour = 'darkblue'
+text_colur = 'white'
 
 # Root dimensions based on the number of players and button dimensions
 root_height = 400
-root_width = 400
+root_width = 200
 
 # Initialize the root window
 root = tkinter.Tk()
@@ -37,7 +39,7 @@ centerWidget(root, root_width, root_height)
 # Create and pack buttons for each player
 for player_name in players.keys():
     func = partial(sourceFunction, name=player_name)
-    button = tkinter.Button(root, command=func, text=f'{player_name} \nCards', font = ('Consolas', 14), bg = 'darkred', fg='white')
+    button = tkinter.Button(root, command=func, text=f'{player_name} \nCards', font = ('Consolas', 14), bg = button_colour, fg='white')
     button.config(width=button_width, height=button_height)
     button.pack(fill= 'both',  expand= True) 
 
