b6011c1362e4c673eabe7d93f61dd06b9c0bb80d
wtc <wtc@pop-os.localdomain>
2024-09-26 13:23:46 +0200

    ok

--- Uno_Game/file.py
+++ Uno_Game/file.py
@@ -1,16 +1,17 @@
-import tkinter as tk
+import tkinter
 
 
 cards = [(9, 'white'), (1, 'white'), ['W_2', 'brown'], ('act_1', 'white'), (1, 'white'), (3, 'white'), ('act_2', 'white')]
 
-root = tk.Tk()
+root = tkinter.Tk()
 root.geometry('300x300')
 
 for card in cards:
-  colour = card[1].lower()
-  frame = tk.Frame(root, bg= colour, width=200, height=100)
+  frame_colour = card[1].lower()
+  frame = tkinter.Frame(root, bg = frame_colour, width=200, height=100)
   frame.pack(fill = 'both', expand= True, pady=10)
 
+  label = tkinter.Label
 
 
 root.mainloop()
