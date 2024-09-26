ce5e4c3f9585baa1e1d9afb560e5613ef3f203d4
wtc <wtc@pop-os.localdomain>
2024-09-26 15:37:27 +0200

    ok

--- Uno_Game/file.py
+++ Uno_Game/file.py
@@ -8,7 +8,7 @@ root.geometry('400x400')
 # FRAME 1
 # Adding black border with highlightthickness and bd
 frame_back_colour = 'darkgrey'
-frame = tkinter.Frame(root, bg = frame_back_colour, width=200, height=100, highlightbackground='black', highlightthickness=2, bd=2)
+frame = tkinter.Frame(root, bg = frame_back_colour, width=100, height=200, highlightbackground='black', highlightthickness=2, bd=2)
 frame.pack_propagate(False)
 
 label = tkinter.Label(frame, text= f'1', bg = 'red', font=('Arial', 24))
@@ -19,11 +19,11 @@ frame.grid(row= 2, column=3, padx=10, pady=10)
 # FRAME 2
 # Adding black border with highlightthickness and bd
 frame_back_colour = 'darkgrey'
-frame2 = tkinter.Frame(root, bg = frame_back_colour, width=200, height=100, highlightbackground='black', highlightthickness=2, bd=2)
+frame2 = tkinter.Frame(root, bg = frame_back_colour, width=100, height=200, highlightbackground='black', highlightthickness=2, bd=2)
 frame2.pack_propagate(False)
 
 label = tkinter.Label(frame2, text= f'2', bg = 'red', font=('Arial', 24))
-label_2.pack(fill = 'both', expand = True)
+label.pack(fill = 'both', expand = True)
 
 frame2.grid(row= 3, column=2, padx=10, pady=10)
 
