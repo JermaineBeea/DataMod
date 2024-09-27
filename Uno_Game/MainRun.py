0f4f1522fa89badf1de7b068239273d16a2cf301
wtc <wtc@pop-os.localdomain>
2024-09-27 11:04:05 +0200

    files added

--- Uno_Game/MainRun.py
+++
@@ -1,7 +0,0 @@
-from GameModules.GeneratePlayers import RunGeneratePlayers
-from GameModules.DrawCard import RunDrawCards
-from GameModules.PlayerTurn import RunPlayerTurn
-
-RunGeneratePlayers()
-RunDrawCards()
-RunPlayerTurn()
